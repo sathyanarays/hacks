107b4946917813a5a4cac38f63d9761c5a4bf20f
Sathyanarayanan Saravanamuthu <sathya.chozhanaadan@gmail.com>
2024-02-17 11:51:50 +0530

    More changes

---
+++ pytorch/quickstart.py
@@ -0,0 +1,21 @@
+#https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
