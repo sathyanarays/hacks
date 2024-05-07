53110c74d66b4678ec00998edacc89aab9ba4b76
Sathyanarayanan Saravanamuthu <sathya.chozhanaadan@gmail.com>
2024-05-07 08:59:35 +0530

    Adding more changes

---
+++ deeplearning/magnuselkman/chapter7/cifar.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+import logging
+
+tf.get_logger().setLevel(logging.ERROR)
+
+cifar_dataset = keras.datasets.cifar10
+(train_images, train_labels),  (test_images, test_labels) = cifar_dataset.load_data()
+
+print("Category: ", train_labels[100])
+plt.figure(figsize=(1,1))
+plt.imshow(train_images[100])
+plt.show()
+
+print(train_images.shape)
+print(train_labels.shape)
