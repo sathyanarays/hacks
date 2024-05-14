9289d5c7a0131728ae4311afc57187c586b7ace7
Sathyanarayanan Saravanamuthu <sathya.chozhanaadan@gmail.com>
2024-05-14 16:50:05 +0530

    Adding more changes

--- serve/kserve/custom_model/model.py
+++ serve/kserve/custom_model/model.py
@@ -6,6 +6,11 @@ import torch
 import numpy as np
 import kserve
 from kserve import Model, ModelServer
+from transformers import AutoProcessor, AutoModelForVision2Seq
+import base64
+from PIL import Image
+import io
+import uuid
 
 class AlexNetModel(Model):
     def __init__(self, name: str):
@@ -14,27 +19,31 @@ class AlexNetModel(Model):
        self.load()
 
     def load(self):
-        self.model = models.alexnet(pretrained=True)
-        self.model.eval()
+        self.model = AutoModelForVision2Seq.from_pretrained(
+            "HuggingFaceM4/idefics2-8b-base",
+        ).to("cpu")
+        self.processor = AutoProcessor.from_pretrained("HuggingFaceM4/idefics2-8b-base")        
         self.ready = True
 
     def predict(self, payload: Dict, headers: Dict[str, str] = None) -> Dict:
         img_data = payload["instances"][0]["image"]["b64"]
         raw_img_data = base64.b64decode(img_data)
         input_image = Image.open(io.BytesIO(raw_img_data))
-        preprocess = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225]),
-        ])
-        input_tensor = preprocess(input_image).unsqueeze(0)
-        output = self.model(input_tensor)
-        torch.nn.functional.softmax(output, dim=1)
-        values, top_5 = torch.topk(output, 5)
-        result = values.flatten().tolist()
-        response_id = generate_uuid()
+
+        input_text = payload["instances"][0]["text"]
+
+        prompts = [input_text]  
+        images = [[input_image]]
+        inputs = self.processor(text=prompts, images=images, padding=True, return_tensors="pt")
+        inputs = {k: v.to("cpu") for k, v in inputs.items()}
+
+
+# Generate
+        generated_ids = self.model.generate(**inputs, max_new_tokens=500)
+        generated_texts = self.processor.batch_decode(generated_ids, skip_special_tokens=True)        
+
+        result = generated_texts
+        response_id = str(uuid.uuid4())
         return {"predictions": result}
 
 if __name__ == "__main__":
