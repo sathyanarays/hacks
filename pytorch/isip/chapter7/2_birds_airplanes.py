a0cbe1aeb05a75fe5cd7b916889300965d79e18a
Sathyanarayanan Saravanamuthu <sathya.chozhanaadan@gmail.com>
2024-07-24 11:34:40 +0530

    Adding files

--- pytorch/isip/chapter7/2_birds_airplanes.py
+++ pytorch/isip/chapter7/2_birds_airplanes.py
@@ -42,4 +42,80 @@ model = nn.Sequential(
 )
 
 def softmax(x):
-    return torch.exp(x) / torch.exp(x).sum
+    return torch.exp(x) / torch.exp(x).sum()
+
+x = torch.tensor([1.0,2.0,3.0])
+print(softmax(x))
+
+print(softmax(x).sum())
+
+softmax = nn.Softmax(dim=1)
+x = torch.tensor([
+    [1.0,2.0,3.0],
+    [1.0,2.0,3.0],
+])
+
+print(softmax(x))
+
+model = nn.Sequential(
+    nn.Linear(3072,512),
+    nn.Tanh(),
+    nn.Linear(512,2),
+    nn.Softmax(dim=1)
+)
+
+img, _ =cifar2[0]
+
+plt.imshow(img.permute(1,2,0))
+plt.savefig('test.png')
+
+img_batch = img.view(-1).unsqueeze(0)
+
+out = model(img_batch)
+print(out)
+
+_, index = torch.max(out, dim=1)
+print(index)
+
+model = nn.Sequential(
+            nn.Linear(3072, 512),
+            nn.Tanh(),
+            nn.Linear(512, 2),
+            nn.LogSoftmax(dim=1))
+
+img, label = cifar2[0]
+
+out = model(img.view(-1).unsqueeze(0))
+print(out)
+
+loss = nn.NLLLoss()
+print(loss(out, torch.tensor([label])))
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+model = nn.Sequential(
+            nn.Linear(3072, 512),
+            nn.Tanh(),
+            nn.Linear(512, 2),
+            nn.LogSoftmax(dim=1))
+
+learning_rate = 1e-2
+
+optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+loss_fn = nn.NLLLoss()
+
+n_epochs = 100
+
+for epoch in range(n_epochs):
+    for img, label in cifar2:
+        out = model(img.view(-1).unsqueeze(0))
+        loss = loss_fn(out, torch.tensor([label]))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print("Epoch: %d, Loss: %f" % (epoch, float(loss)))
+    
