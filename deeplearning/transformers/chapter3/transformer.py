7c4485537490bcb4106c1cfb95bdd85b320447a6
Sathyanarayanan Saravanamuthu <ssaravanamut@nvidia.com>
2024-08-27 15:29:34 +0530

    Adding more changes

--- deeplearning/transformers/chapter3/transformer.py
+++ deeplearning/transformers/chapter3/transformer.py
@@ -4,7 +4,7 @@ from transformers import AutoTokenizer
 model_ckpt = "distilbert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
 
-text = "this is not a test"
+text = "this is not a test but production"
 inputs = tokenizer(text, return_tensors="pt", add_special_tokens=False)
 
 
@@ -109,7 +109,39 @@ class TransformerEncoderLayer(nn.Module):
         x = x + self.feed_forward(self.layer_norm_2(x))
         return x
 
-print("\n### TransformerEncoderLayer")
-encoder_layer = TransformerEncoderLayer(config)
-print(inputs_embeds.shape)
-print(encoder_layer(inputs_embeds).size())
+
+class Embeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.token_embeddings = nn.Embedding(config.vocab_size, config.hidden_size)
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        self.layer_norm = nn.LayerNorm(config.hidden_size, eps=1e-12)
+        self.dropout = nn.Dropout()
+
+    def forward(self, input_ids):
+        # Create position IDs for input sequence
+        seq_length = input_ids.size(1)
+        position_ids = torch.arange(seq_length, dtype=torch.long).unsqueeze(0)
+        # Create token and position embeddings
+        token_embeddings = self.token_embeddings(input_ids)
+        position_embeddings = self.position_embeddings(position_ids)
+        # Combine token and position embeddings
+        embeddings = token_embeddings + position_embeddings
+        embeddings = self.layer_norm(embeddings)
+        embeddings = self.dropout(embeddings)
+        return embeddings
+    
+class TransformerEncoder(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.embeddings = Embeddings(config)
+        self.layers = nn.ModuleList([TransformerEncoderLayer(config) for _ in range(config.num_hidden_layers)])
+
+    def forward(self, x):
+        x = self.embeddings(x)
+        for layer in self.layers:
+            x = layer(x)
+        return x
+    
+encoder = TransformerEncoder(config)
+print(encoder(inputs.input_ids).size())
